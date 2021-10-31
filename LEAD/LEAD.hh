9b05c15682147d90316b12903fb8cb4a25a108c9
Vito Wu <117010285@Link.cuhk.edu.cn>
2021-10-31 15:10:03 +0800

    Add LEAD file to implementation

---
+++ LEAD/LEAD.hh
@@ -0,0 +1,8 @@
+//
+// Created by Vito Wu on 31/10/2021.
+//
+
+#ifndef ZIGBEETESTBED_LEAD_HH
+#define ZIGBEETESTBED_LEAD_HH
+
+#endif //ZIGBEETESTBED_LEAD_HH
---
+++ LEAD/LEAD_Send.cc
@@ -0,0 +1,4 @@
+//
+// Created by Vito Wu on 31/10/2021.
+//
+
